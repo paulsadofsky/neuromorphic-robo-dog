18677d766119c6d59e224f206e4080122aea0bc2
paulsadofsky <74940533+paulsadofsky@users.noreply.github.com>
2024-06-27 20:05:09 +0000

    Doesn't work but UGH

--- Neuron Tests/Fourth Order/fourth-order-neuron.cpp
+++ Neuron Tests/Fourth Order/fourth-order-neuron.cpp
@@ -10,12 +10,12 @@ double ext_current = -2;
 
 // Set value of C and time step
 double c = 0.0009;
-double dt = 12;
+double dt = 0.0001;
 
 // Sets the three time constant values, with each increasing index correlating to f, s and us respectively
 // Initiallizes each voltage with the same respective index assignment
 double tau_x[3] = {5.0, 250.0, 12500.0};
-double v_x[3];
+double v_x[3] = {v_reset, v_reset, v_reset};
 
 // Sets the four alpha, beta, and delta values, with each increasing index correlating to fn, sp, sn and usp respectively
 // Initiallizes each current with the same respective index assignment
@@ -27,45 +27,53 @@ double i_sum = 0.0;
 double v_temp = 0.0;
 
 int main() {
-    // Calculates each value of i_x and the summation
-    for (int i = 0; i < 4; i++) {
-        switch (i) {
-            case (0): { v_temp = -v_x[0]; }
-            case (1): { v_temp = v_x[1]; }
-            case (2): { v_temp = -v_x[1]; }
-            case (3): { v_temp = v_x[2]; }
-        }
-        
-        double lower_bound = -(alpha_x[i]/beta_x[i]) + delta_x[i];
-        double upper_bound = (alpha_x[i]/beta_x[i]) + delta_x[i];
-        if (v_temp < lower_bound) {
-            i_x[i] = -alpha_x[i];
-        }
-        else if (v_temp > upper_bound) {
-            i_x[i] = alpha_x[i];
-        }
-        else {
+    for (int a = 0; a < 1000; a++) {
+        for (int i = 0; i < 4; i++) {
+            i_sum = 0;
+
+            // Calculates each value of v_x
+            for(int k = 0; k < 3; k++) {
+                v_x[k] += (tau_x[k]/dt)*(v_mem - v_x[k]);
+            }
+
+            // Sets the appropriate value of v_x and it's corresponding sign
+            switch (i) {
+                case (0): { v_temp = v_x[0]; }
+                case (1): { v_temp = v_x[1]; }
+                case (2): { v_temp = v_x[1]; }
+                case (3): { v_temp = v_x[2]; }
+            }
             
-            i_x[i] = beta_x[i]*(v_temp - delta_x[i]);
+            // Calculates each value of i_x and the summation
+            double lower_bound = -(alpha_x[i]/beta_x[i]) + delta_x[i];
+            double upper_bound = (alpha_x[i]/beta_x[i]) + delta_x[i];
+            if (v_temp < lower_bound) {
+                i_x[i] = -alpha_x[i];
+            }
+            else if (v_temp > upper_bound) {
+                i_x[i] = alpha_x[i];
+            }
+            else {
+                i_x[i] = beta_x[i]*(v_temp - delta_x[i]);
+            }
+
+            if (i == 0 || i == 2) {
+                i_x[i] = i_x[i] * -1;
+            }
+
+            i_sum += i_x[i];
         }
 
-        i_sum += i_x[i];
+        v_mem += (dt / c) * (ext_current - v_mem - i_sum);
+        cout << v_mem << " ";
 
-        // Calculates each value of v_x
-        if (i != 3) {
-            v_x[i] += (tau_x[i]/dt)*(v_mem - v_x[i]);
+        // Check for spike and resets the membrane potential if spike occurs
+        if (v_mem >= v_thresh) {
+            v_mem = v_reset;
+            cout << "\nSpike" << endl;
         }
+        cout << "\n";
     }
-
-    v_mem += (dt / c) * (ext_current - v_mem - i_sum);
-    cout << v_mem << " ";
-
-    // Check for spike and resets the membrane potential if spike occurs
-    if (v_mem >= v_thresh) {
-        v_mem = v_reset;
-        cout << "\nSpike" << endl;
-    }
-    cout << "\n";
 
     return 0;
 }
