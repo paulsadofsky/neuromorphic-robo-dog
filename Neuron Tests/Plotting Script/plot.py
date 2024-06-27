6f0be03d4b32183c63123d672b31a5b9621213c3
Florian Reihl <59579743+Florian268@users.noreply.github.com>
2024-06-27 10:40:08 -0400

    added plot.py

---
+++ Neuron Tests/Plotting Script/plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+time = df['Time']
+voltage = df['Membrane Voltage']
+
+plt.figure(figsize=(10, 6))
+plt.plot(time, voltage)
+plt.xlabel('Time')
+plt.ylabel('Membrane Voltage (Vmem) [a.u.]')
+plt.title('Bio-Mimetic Neuron Simulation on Loihi')
+plt.grid(True)
+
+plt.annotate('Spiking', xy=(2500, -4), xytext=(2500, -4.5), arrowprops=dict(arrowstyle='<->'))
+plt.annotate('Bursting', xy=(12500, -4), xytext=(12500, -4.5), arrowprops=dict(arrowstyle='<->'))
+
+plt.show()
