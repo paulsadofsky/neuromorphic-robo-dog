48df409f58a6364ce833ccd04859332865239658
paulsadofsky <74940533+paulsadofsky@users.noreply.github.com>
2024-07-12 17:42:39 +0000

    MTF neuron implementation

--- FPGA/mtf-neuron/mtf_neuron.v
+++ FPGA/mtf-neuron/mtf_neuron.v
@@ -3,6 +3,7 @@ module MTF_neuron (
     input wire reset,
     input wire [7:0] i_ext,
     input wire [7:0] thresh,
+    input wire [7:0] dt,
     input wire [7:0] alpha [3:0],
     input wire [7:0] delta [3:0],
     input wire [15:0] tau [2:0],
@@ -10,6 +11,7 @@ module MTF_neuron (
     output reg [7:0] voltage);
 
     reg [7:0] current_sum;
+    reg [15:0] v_temp;
     reg [15:0] v_x [2:0];
     reg [15:0] i_x [3:0];
 
@@ -24,6 +26,7 @@ module MTF_neuron (
             i_x[0] = 16'd0;
 
             voltage <= 8'd0;
+            v_temp <= 8'd0;
 
             current_sum <= 8'd0;
             spike <= 0;
@@ -35,11 +38,30 @@ module MTF_neuron (
 
     // Calculating v_x values
     always @(posedge clk) begin
-        
+        for (int i = 0; i < 3; i++) begin
+            v_x[i] <= v_x[i] + ((dt/tau[i])*(voltage - v_x[i]));
+        end
     end
 
     // Calculating i_x values
     always @(posedge clk) begin
+        current_sum <= 8'd0;
         
+        for (int j = 0; j < 3; j++) begin
+            if (j <= 1) begin v_temp <= v_x[j]; end
+            else begin v_temp <= v_x[j-1]; end
+
+            if (v_temp < (-1 + delta[j])) begin
+                i_x[j] <= -1*alpha[j];
+            end
+            else if (v_temp > 1 + delta[j]) begin
+                i_x[j] <= alpha[j];
+            end
+            else begin
+                i_x[j] <= alpha[j]*(v_temp - delta[j]);
+            end
+
+            current_sum <= current_sum + i_x[j];
+        end
     end
 endmodule
--- FPGA/mtf-neuron/mtf_neuron_tb.v
+++ FPGA/mtf-neuron/mtf_neuron_tb.v
@@ -5,6 +5,10 @@ module lif_tb ();
      reg reset;
      reg [7:0] i_ext = 8'd10;
      reg [7:0] thresh = 8'd20;
+     reg [7:0] dt;
+     reg [7:0] alpha [3:0];
+     reg [7:0] delta [3:0];
+     reg [15:0] tau [2:0];
      wire spike;
      wire [7:0] voltage;
     
@@ -15,15 +19,37 @@ module lif_tb ();
         .reset(reset),
         .i_ext(i_ext),
         .thresh(thresh),
+        .dt(dt),
+        .alpha(alpha),
+        .delta(delta),
+        .tau(tau),
         .spike(spike),
         .voltage(voltage)
     );
+
     initial
     begin
         clk = 1'b0;
+
+        dt <= ((8'd1) >> 1);            // 0.5
+
+        alpha[0] <= -1*8'd2;            // -2
+        alpha[1] <= 8'd2;               // 2
+        alpha[2] <= ((-1*8'd3) >> 1);   // -1.5
+        alpha[3] <= ((8'd3) >> 1);      // 1
+
+        delta[0] <= 8'd0;               // 0
+        delta[1] <= 8'd0;               // 0
+        delta[2] <= ((-1*8'd3) >> 1);   // -1.5
+        delta[3] <= ((-1*8'd3) >> 1);   // -1.5
+
+        tau[0] <= 15'd1;                // 1
+        tau[1] <= 15'd50;               // 50
+        tau[2] <= 15'd2500;             // 2500
+
         forever
         begin
-            #1 clk = ~clk;
+            #1 clk <= ~clk;
         end
     end 
     
@@ -31,14 +57,11 @@ module lif_tb ();
     begin
         fspike = $fopen("spike.txt","w");
 
-        reset = 1'b1;
+        reset <= 1'b1;
         # 10
-        reset = 1'b0;
-        i_ext = 8'd10;
-        # 300
-        i_ext = 8'd4;
-        # 300
-        i_ext = 8'd1;
+        reset <= 1'b0;
+
+        i_ext <= ((-1*8'd3) >> 1);
         # 300
         $fclose(fspike);  
         $finish;
@@ -46,7 +69,6 @@ module lif_tb ();
     
     always @(posedge spike)
     begin
-//        $display("clk out%d\n",   state);
         $fwrite(fspike,"%t\n",   $realtime);
     end
 endmodule
