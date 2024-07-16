13cc189e177608c413eaa16969e78f1936c56a2e
ShaazNadeem <san120@pitt.edu>
2024-07-16 15:02:41 +0000

    Change

--- Neuron Tests/Fourth Order/single-neuron-network/plot/plot.py
+++ Neuron Tests/Fourth Order/single-neuron-network/plot/plot.py
@@ -14,7 +14,7 @@ plt.title('Bio-Mimetic Neuron Simulation on Loihi')
 plt.grid(True)
 
 #plt.annotate('Spiking', xy=(2500, -4), xytext=(2500, -4.5), arrowprops=dict(arrowstyle='<->'))
-#plt.annotate('Bursting', xy=(12500, -4), xytext=(12500, -4.5), arrowprops=dict(arrowstyle='<->'))
+plt.annotate('Bursting', xy=(12500, -4), xytext=(12500, -4.5), arrowprops=dict(arrowstyle='<->'))
 
 plt.savefig('Neuron Tests/Fourth Order/single-neuron-network/plot/plot.png')
 plt.show()
