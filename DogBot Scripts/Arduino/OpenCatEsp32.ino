effc259dbdcd33be66eba1dd6ac23d3a5f4d15a5
paulsadofsky <74940533+paulsadofsky@users.noreply.github.com>
2024-07-22 15:31:05 -0400

    Update OpenCatEsp32.ino

--- DogBot Scripts/Arduino/OpenCatEsp32.ino
+++ DogBot Scripts/Arduino/OpenCatEsp32.ino
@@ -24,14 +24,12 @@
 #define GESTURE                   // for Gesture module
 #define CAMERA                    // for Mu Vision camera
 #define QUICK_DEMO                // for quick demo
-// #define ROBOTIC_ARM               // for attaching head clip arm
 #include "src/OpenCat.h"
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);  // USB serial
   Serial.setTimeout(SERIAL_TIMEOUT);
-  //  Serial1.begin(115200); //second serial port
   while (Serial.available() && Serial.read())
     ;  // empty buffer
   initRobot();
@@ -42,96 +40,118 @@ void loop() {
 #ifdef VOLTAGE
   lowBattery();
 #endif
-  //  //—self-initiative
-  //  if (autoSwitch) { //the switch can be toggled on/off by the 'z' token
-  //    randomMind();//allow the robot to auto rest and do random stuff in randomMind.h
-  //    powerSaver(POWER_SAVER);//make the robot rest after a certain period, the unit is seconds
-  //
-  //  }
   //  //— read environment sensors (low level)
   readEnvironment();
-  //  //— special behaviors based on sensor events
   dealWithExceptions();  // low battery, fall over, lifted, etc.
   if (!tQueue->cleared()) {
     tQueue->popTask();
   } else {
     readSignal();
-//#ifdef QUICK_DEMO
-//    if (moduleList[moduleIndex] == EXTENSION_QUICK_DEMO)
-//      quickDemo();
-//#endif
-    //  readHuman();
   }
-  //  //— generate behavior by fusing all sensors and instruction
-  //  decision();
-
-  //  //— action
-  //  //playSound();
 #ifdef NEOPIXEL_PIN
   playLight();
 #endif
   reaction();
 }
-//
-//#ifdef QUICK_DEMO  // enter XQ in the serial monitor to activate the following section
-//int prevReading = 0;
-//void quickDemo() {  // this is an example that use the analog input pin ANALOG1 as a touch pad
-//  // if the pin is not connected to anything, the reading will be noise
-//  int currentReading = analogRead(ANALOG1);
-//  if (abs(currentReading - prevReading) > 50)  // if the reading is significantly different from the previous reading
-//  {
-//    PT("Reading on pin ANALOG1:\t");
-//    PTL(currentReading);
-//    if (currentReading < 50) {                                        // touch and hold on the A2 pin until the condition is met
-//      tQueue->addTask(T_BEEP, "12 4 14 4 16 2");                      // make melody
-//      tQueue->addTask(T_INDEXED_SEQUENTIAL_ASC, "0 30 0 -30", 1000);  // move the neck, left shoulder, right shoulder one by one
-//    } else if (abs(currentReading - prevReading) < 100) {
-//      if (strcmp(lastCmd, "sit"))
-//        tQueue->addTask(T_SKILL, "sit", 1000);  // make the robot sit. more tokens are defined in OpenCat.h
-//    } else {
-//      if (strcmp(lastCmd, "up"))
-//        tQueue->addTask(T_SKILL, "up", 1000);  // make the robot stand up. more tokens are defined in OpenCat.h
-//    }
-//  }
-//  prevReading = currentReading;
-//}
-//#endif
 
-#ifdef QUICK_DEMO  // enter XQ in the serial monitor to activate the following section
+// enter XQ in the serial monitor to activate the following section
+#ifdef QUICK_DEMO
 void quickDemo() {
-  //tQueue->addTask(T_BEEP, "12 4 14 4 16 2");                      // make melody
-  //tQueue->addTask(T_SKILL, "sit", 5000);                            // make the robot sit. more tokens are defined in OpenCat.h
-  
-  tQueue->addTask(T_SKILL, "up", 5000);                             // make the robot stand up. more tokens are defined in OpenCat.h
 
-  for(int i = 0; i<10; i++) {
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 10, 9, 29, 10, 51, 11, 46, 12, 33, 13, 30, 14, 4, 15, 15", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 9, 9, 31, 10, 48, 11, 47, 12, 41, 13, 29, 14, 3, 15, 16", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 10, 9, 32, 10, 43, 11, 48, 12, 40, 13, 29, 14, 3, 15, 16", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 14, 9, 33, 10, 38, 11, 48, 12, 36, 13, 30, 14, 6, 15, 17", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 15, 9, 35, 10, 34, 11, 49, 12, 35, 13, 30, 14, 13, 15, 18", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 17, 9, 38, 10, 35, 11, 51, 12, 34, 13, 28, 14, 16, 15, 18", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 19, 9, 44, 10, 36, 11, 52, 12, 33, 13, 17, 14, 14, 15, 19", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 20, 9, 42, 10, 38, 11, 52, 12, 32, 13, 16, 14, 14, 15, 20", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 22, 9, 38, 10, 39, 11, 53, 12, 31, 13, 16, 14, 14, 15, 22", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 24, 9, 35, 10, 41, 11, 59, 12, 31, 13, 16, 14, 14, 15, 16", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 25, 9, 30, 10, 42, 11, 61, 12, 30, 13, 17, 14, 14, 15, 11", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 27, 9, 26, 10, 43, 11, 59, 12, 30, 13, 19, 14, 15, 15, 8", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 28, 9, 21, 10, 44, 11, 56, 12, 30, 13, 21, 14, 15, 15, 6", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 29, 9, 15, 10, 46, 11, 52, 12, 30, 13, 27, 14, 15, 15, 5", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 31, 9, 8, 10, 47, 11, 49, 12, 29, 13, 40, 14, 16, 15, 3", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 32, 9, 10, 10, 48, 11, 45, 12, 29, 13, 40, 14, 16, 15, 3", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 33, 9, 13, 10, 48, 11, 41, 12, 30, 13, 37, 14, 17, 15, 3", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 35, 9, 15, 10, 49, 11, 35, 12, 30, 13, 36, 14, 18, 15, 10", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 38, 9, 16, 10, 51, 11, 33, 12, 28, 13, 34, 14, 18, 15, 17", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 44, 9, 19, 10, 52, 11, 35, 12, 17, 13, 33, 14, 19, 15, 16", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 42, 9, 20, 10, 52, 11, 37, 12, 16, 13, 32, 14, 20, 15, 14", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 38, 9, 21, 10, 53, 11, 39, 12, 16, 13, 32, 14, 22, 15, 14", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 35, 9, 23, 10, 59, 11, 40, 12, 16, 13, 31, 14, 16, 15, 14", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 30, 9, 25, 10, 61, 11, 42, 12, 17, 13, 30, 14, 11, 15, 14", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 26, 9, 26, 10, 59, 11, 42, 12, 19, 13, 30, 14, 8, 15, 14", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 21, 9, 28, 10, 56, 11, 44, 12, 21, 13, 30, 14, 6, 15, 15", 0);
-    tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, "8, 15, 9, 28, 10, 52, 11, 45, 12, 27, 13, 30, 14, 5, 15, 15", 0);
+  // Make the robot stand up
+  tQueue->addTask(T_SKILL, "up", 5000);
+
+  String clock = "_736_1001_202_1001_169_1001_165_1001_162_1001_161_1001_163_1001_189_1001_237_1001_633_0110_185_0110_154_0110_154_0110_152_0110_153_0110_156_0110_183_0110_245_0110_1442_1001_203_1001_170_1001_165_1001_163_1001_161_1001_161_1001_182_1001_226_1001_1220_0110_179_0110_153_0110_153_0110_152_0110_160_0110_195_0110_279_0110_401_";
+  int zeroActivation[100];
+  int activation[100][4];
+  int indexCounter = 0;
+  
+  for (int i = 0; i < clock.length(); i+=9) {
+    zeroActivation[indexCounter] = clock.substring(i+1,i+4).toInt();
+    activation[indexCounter][0] = clock.substring(i+5,i+6).toInt();
+    activation[indexCounter][1] = clock.substring(i+6,i+7).toInt();
+    activation[indexCounter][2] = clock.substring(i+7,i+8).toInt();
+    activation[indexCounter][3] = clock.substring(i+8,i+9).toInt();
+    indexCounter++;
+  }
+  zeroActivation[indexCounter] = clock.substring(clock.length()-4, clock.length()-1).toInt();
+
+  int motorAngles[27][8] = {{10, 29, 51, 46, 33, 30, 4,  15},
+                            {9,  31, 48, 47, 41, 29, 3,  16},
+                            {10, 31, 43, 48, 40, 29, 3,  16},
+                            {14, 33, 38, 48, 36, 30, 6,  17},
+                            {15, 35, 34, 49, 35, 30, 13, 18},
+                            {17, 38, 35, 51, 34, 28, 16, 18},
+                            {19, 44, 36, 52, 33, 17, 14, 19},
+                            {20, 42, 38, 52, 32, 16, 14, 20},
+                            {22, 38, 39, 53, 31, 16, 14, 22},
+                            {24, 35, 41, 59, 31, 16, 14, 16},
+                            {25, 30, 42, 61, 30, 17, 14, 11},
+                            {27, 26, 43, 59, 30, 19, 15, 8 },
+                            {28, 21, 44, 56, 30, 21, 15, 6 },
+                            {29, 15, 46, 52, 30, 27, 15, 5 },
+                            {31, 8,  47, 49, 29, 40, 16, 3 },
+                            {32, 10, 48, 45, 29, 40, 16, 3 },
+                            {33, 13, 48, 41, 30, 37, 17, 3 },
+                            {35, 15, 49, 35, 30, 36, 18, 10},
+                            {38, 16, 51, 33, 28, 34, 18, 17},
+                            {44, 19, 52, 35, 17, 33, 19, 16},
+                            {42, 20, 52, 37, 16, 32, 20, 14},
+                            {38, 21, 53, 39, 16, 32, 22, 14},
+                            {35, 23, 59, 40, 16, 31, 16, 14},
+                            {30, 25, 61, 42, 17, 30, 11, 14},
+                            {26, 26, 59, 42, 19, 30, 8,  14},
+                            {21, 28, 56, 44, 21, 30, 6,  15},
+                            {15, 28, 52, 45, 27, 30, 5,  15}};
+
+  int zeroCounter = 0;
+  int neuronAngleCount[4] = {0,0,0,0};
+  String tempArgs = "";
+  
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < indexCounter; j++) {
+      for (int k = 0; k < zeroActivation[j]; k++) {
+        // Do nothing (waste clock cycles)
+      }
+      
+      if (activation[j][0] == 1) {  // FL neuron activation
+        for (int k = 0; k < 3; k++) {
+          tempArgs = "8, " + String(motorAngles[neuronAngleCount[0]][0]) + ", 9, " + String(motorAngles[neuronAngleCount[0]][1]);
+          tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, tempArgs.c_str(), 0);
+          neuronAngleCount[0]++;
+        }
+      }
+      
+      if (activation[j][1] == 1) {  // FR neuron activation
+        for (int k = 0; k < 3; k++) {
+          tempArgs = "10, " + String(motorAngles[neuronAngleCount[1]][2]) + ", 11, " + String(motorAngles[neuronAngleCount[1]][3]);
+          tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, tempArgs.c_str(), 0);
+          neuronAngleCount[1]++;
+        }
+      }
+      
+      if (activation[j][2] == 1) {  // BL neuron activation
+        for (int k = 0; k < 3; k++) {
+          tempArgs = "12, " + String(motorAngles[neuronAngleCount[2]][4]) + ", 13, " + String(motorAngles[neuronAngleCount[2]][5]);
+          tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, tempArgs.c_str(), 0);
+          neuronAngleCount[2]++;
+        }
+      }
+      
+      if (activation[j][3] == 1) {  // BR neuron activation
+        for (int k = 0; k < 3; k++) {
+          tempArgs = "14, " + String(motorAngles[neuronAngleCount[3]][6]) + ", 15, " + String(motorAngles[neuronAngleCount[3]][7]);
+          tQueue->addTask(T_INDEXED_SIMULTANEOUS_ASC, tempArgs.c_str(), 0);
+          neuronAngleCount[3]++;
+        }
+      }
+
+      for (int k = 0; k < 4; k++) {
+        if(neuronAngleCount[k] == 27) {
+          neuronAngleCount[k] = 0;
+        }
+      }
+    }
   }
 }
 #endif
